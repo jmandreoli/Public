f8e06e780ce6b3e6ce3fadc35897a6173fdad9b1
Jean-Marc Andreoli <jean-marc.andreoli@naverlabs.com>
2022-09-19 10:32:43 +0200

    Updates

--- mha.py
+++ mha.py
@@ -10,11 +10,11 @@ MHA = torch.nn.MultiheadAttention(embed_dim=12,num_heads=4,batch_first=True)
 #MHA = torch.nn.MultiheadAttention(embed_dim=12,num_heads=4,batch_first=True,kdim=17,vdim=19)
 B = 13; M = 10; N = 8 # batch size, input size, output size
 output_shape = (B,N,MHA.embed_dim) # expected shape of the result
+query_input = torch.rand(B,N,MHA.embed_dim) # query
+key_input = torch.rand(B,M,MHA.kdim) # key
+value_input =  torch.rand(B,M,MHA.vdim) # value
 biases = dict(zip(('query','key','value'),torch.chunk(MHA.in_proj_bias.data,3)))
 def test(bias_name='key'): # either 'key', 'query' or 'value'
-  query_input = torch.rand(B,N,MHA.embed_dim) # query
-  key_input = torch.rand(B,M,MHA.kdim) # key
-  value_input =  torch.rand(B,M,MHA.vdim) # value
   with torch.no_grad():
     # Retrieve the bias
     bias = biases[bias_name]
@@ -27,7 +27,8 @@ def test(bias_name='key'): # either 'key', 'query' or 'value'
     # and collect the result with the same input
     y2,_ = MHA(query_input,key_input,value_input)
     assert y1.shape == output_shape and y2.shape == output_shape # sanity check
-    print(f'Output shape: {output_shape}')
   # observe that with bias_name='key', y1 and y2 and essentially identical; not with the other two
-  print(f'Delta[{bias_name}] = {torch.max(abs(y2-y1)).item():.2g}')
+  print(f'Output shape: {output_shape}; Delta[{bias_name}] = {torch.max(abs(y2-y1)).item():.2g}')
+if __name__=='__main__':
+  for bias_name in 'query','key','value': test(bias_name)
 
